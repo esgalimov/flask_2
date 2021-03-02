8316118f4569101fd0687b678f3aa5af2e3fa50a
esgalimov <esgalimov@gmail.com>
2021-03-02 18:44:42 +0300

    WEB. Шаблоны. flask-wtf. Задача 1. Готовимся к миссии. server.py

---
+++ sever.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Заготовка')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
