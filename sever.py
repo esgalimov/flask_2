5fc34d9def30dec443ff32151fd5d6f938a077f4
esgalimov <esgalimov@gmail.com>
2021-03-02 20:26:17 +0300

    WEB. Шаблоны. flask-wtf. Задача 2 (исправлено). Тренировки в полёте. sever.py

--- sever.py
+++ sever.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
 
 
 app = Flask(__name__)
+# css = url_for('static', filename='css/style.css')
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Заготовка')
+
+
+@app.route('/training/<prof>')
+def tran(prof):
+    return render_template('tran.html', title='Заготовка', prof=prof)
 
 
 if __name__ == '__main__':
