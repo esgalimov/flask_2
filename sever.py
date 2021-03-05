a55022f7066cfe9706e686376cc2dc7cd4db00e8
esgalimov <esgalimov@gmail.com>
2021-03-05 20:08:10 +0300

    WEB. Шаблоны. flask-wtf. Задача 3. Список профессий. sever.py

--- sever.py
+++ sever.py
@@ -18,5 +18,16 @@ def tran(prof):
     return render_template('tran.html', title='Заготовка', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def prof_list(list):
+    prof_lis = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог',
+                'врач', 'инженер по терраформированию',
+                'специалист по радиционной защите', 'климатолог',
+                'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
+                'метеоролог', 'оператор марсохода',
+                'киберинженер', 'штурман', 'пилот дронов']
+    return render_template('prof_list.html', title='Заготовка', list=list, prof_lis=prof_lis)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
